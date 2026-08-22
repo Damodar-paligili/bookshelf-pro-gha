79494fb38b05fb5b322a4dcbe46b8f0f7da7e614
damodar-paligili <damodarpp99@gmail.com>
2026-08-22 21:58:33 +0530

    Modify EKS cluster to use public and private subnets
    Updated EKS cluster configuration to include public subnet IDs and enable both private and public endpoint access.

--- ROOT-terraform/modules/eks/main.tf
+++ ROOT-terraform/modules/eks/main.tf
@@ -19,7 +19,9 @@ resource "aws_eks_cluster" "main" {
   name     = var.cluster_name
   role_arn = aws_iam_role.cluster.arn
   vpc_config {
-    subnet_ids = var.private_subnet_ids
+    subnet_ids              = concat(var.public_subnet_ids, var.private_subnet_ids)
+    endpoint_private_access = true
+    endpoint_public_access  = true
   }
   depends_on = [aws_iam_role_policy_attachment.cluster_policy]
 }
